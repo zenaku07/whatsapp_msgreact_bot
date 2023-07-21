95aae01f6c3beb3b659fb438f6cfed09232445a8
Zenaku <125358161+zenaku07@users.noreply.github.com>
2023-07-21 14:22:53 +0530

    Update reaction.js

--- messageController/reaction.js
+++ messageController/reaction.js
@@ -1,7 +1,7 @@
-// Function to react to personal messages with a thumb emoji
+
 async function reactToMessage(msg) {
     try {
-      // React to personal messages with a thumb emoji
+      
       const reaction = await msg.react('🟡');
       console.log('Reaction added:', reaction);
     } catch (error) {
